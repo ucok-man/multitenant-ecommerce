8d984578ae69f9a5cac8881b6bec8d26ff82a447
Alvin Odhi N <ucokkocu411@gmail.com>
2025-05-17 19:44:58 +0700

    feat: theme customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center space-y-4">
+      <Button variant={"elevated"}>Hello</Button>
+      <Input placeholder="Gello" />
+      <Progress value={50} />
+    </div>
+  );
 }
